5810d77e4c0f604644b918153d7595703c4c2a79
JaredTrOr <trujillojaredalexander@gmail.com>
2023-05-09 20:13:05 -0600

    Addition of the basic setup of controllers, it was changed the name of the Home controller and it was implemented the table to show the users view

---
+++ app/Controllers/MainController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+class MainController extends BaseController
+{
+    public function index()
+    {
+        return view('show_list');
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function getName() 
+    {
+        
+    }
+}
--- app/Views/show_list.php
+++ app/Views/show_list.php
@@ -15,7 +15,7 @@
   <body>
     <div class="container">
       <h1>CRUD con Codeigniter 4</h1>
-      <div class="row">
+      <div class="row form-row">
         <div class="col-lg-6">
           <form action="<?php base_url().'/create' ?>" method="POST">
             <div class="form-element-container">
@@ -40,7 +40,24 @@
         <div class="col-lg-6 image-form-container">
           <img src="images/form_image.png" alt="note image">
         </div>
+      </div>
 
+      <div class="row table-row mt-5">
+        <div class="col-sm-12">
+          <h1>Mostrar usuario</h1>
+          <table class="user-table table table-hover table-border">
+            <tr>
+              <th>Nombre</th>
+              <th>Apellido paterno</th>
+              <th>Apellido materno</th>
+              <th>Editar</th>
+              <th>Eliminar</th>
+            </tr>
+          </table>
+        </div>
+
+
+        
       </div>
     </div>
 
@@ -49,5 +66,6 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
   </body>
 </html>
